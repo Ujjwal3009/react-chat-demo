5d59e1c40fb16ce1adc22f76fbf100e9c227ea07
Ujjwal Kumar <50870185+Ujjwal3009@users.noreply.github.com>
2021-07-09 13:33:10 +0530

    Add files via upload

---
+++ build/precache-manifest.8655a18be8064aa64da3d3afe9c6c520.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9c400e1491ea55e37dc506c189f53a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f31dd85fde3dbbaee1d",
+    "url": "./static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "aee4ff78fcea5fa8cfb0",
+    "url": "./static/css/main.967e0c74.chunk.css"
+  },
+  {
+    "revision": "0f31dd85fde3dbbaee1d",
+    "url": "./static/js/2.41dca140.chunk.js"
+  },
+  {
+    "revision": "aee4ff78fcea5fa8cfb0",
+    "url": "./static/js/main.1e5238c7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "./static/media/ic_wave_hand.9299706f.png"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "./static/media/mimi1.477fc8dd.gif"
+  },
+  {
+    "revision": "82a783353a27abe59feafd40bc686af1",
+    "url": "./static/media/mimi2.82a78335.gif"
+  },
+  {
+    "revision": "477fc8ddd40d200fe8f2635b5151a873",
+    "url": "./static/media/mimi3.477fc8dd.gif"
+  },
+  {
+    "revision": "6b65742393c65511ba223ec388c85ecc",
+    "url": "./static/media/mimi4.6b657423.gif"
+  },
+  {
+    "revision": "609127cb5129f8e218fb57f059497c87",
+    "url": "./static/media/mimi5.609127cb.gif"
+  },
+  {
+    "revision": "1ce35a6b3c21cc308a7683dfbea81c63",
+    "url": "./static/media/mimi6.1ce35a6b.gif"
+  },
+  {
+    "revision": "ace546bd302229af3a0c7e7221d2dc10",
+    "url": "./static/media/mimi7.ace546bd.gif"
+  },
+  {
+    "revision": "6646a3dec849498313a17c7de5d2ea5c",
+    "url": "./static/media/mimi8.6646a3de.gif"
+  },
+  {
+    "revision": "b5ec7c32285b24c09eca4646a4511518",
+    "url": "./static/media/mimi9.b5ec7c32.gif"
+  }
+]);
